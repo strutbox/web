1087daad884a7f1dd6721314481e423480d173e6
Matt Robenolt <matt@ydekproductions.com>
2018-08-06 11:37:51 -0700

    Cleanup dead code in push service

--- strut/services/push.py
+++ strut/services/push.py
@@ -19,14 +19,11 @@ class Service:
         strut.setup()
 
         from django.conf import settings
-        from channels import DEFAULT_CHANNEL_LAYER
-        from channels.layers import get_channel_layer
         from channels.routing import get_default_application
         from daphne.access import AccessLogGenerator
         from daphne.server import Server
 
         application = get_default_application()
-        channel_layer = get_channel_layer(DEFAULT_CHANNEL_LAYER)
 
         endpoint = build_endpoint_description_string(self.bind)
 
@@ -34,9 +31,7 @@ class Service:
         Server(
             application=application,
             signal_handlers=True,
-            # channel_layer=channel_layer,
             endpoints=[endpoint],
-            # ws_protocols=getattr(settings, "CHANNELS_WS_PROTOCOLS", None),
             action_logger=AccessLogGenerator(sys.stdout),
             ping_interval=20,
             ping_timeout=30,
@@ -45,5 +40,4 @@ class Service:
             verbosity=1,
             proxy_forwarded_address_header="X-Forwarded-For",
             proxy_forwarded_port_header="X-Forwarded-Port",
-            # proxy_forwarded_proto_header="X-Forwarded-Proto",
         ).run()

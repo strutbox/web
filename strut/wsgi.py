204b5e8ff4e854fcba7fee9e096ca6ca3a9b0ad9
Matt Robenolt <matt@ydekproductions.com>
2018-08-10 11:35:48 -0700

    Add Sentry WSGI middleware too

--- strut/wsgi.py
+++ strut/wsgi.py
@@ -1,8 +1,8 @@
 from django.core.wsgi import get_wsgi_application
+from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
 import strut
 
 strut.setup()
 
-
-application = get_wsgi_application()
+application = Sentry(get_wsgi_application())

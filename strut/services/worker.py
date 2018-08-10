9074b84bc1ea440d4a91aa83dc2bbff3424b6ad7
Matt Robenolt <matt@ydekproductions.com>
2018-08-10 11:15:23 -0700

    Report worker failures to Sentry

--- strut/services/worker.py
+++ strut/services/worker.py
@@ -8,6 +8,24 @@ def get_name(len=8):
     return "".join(choice(alphabet) for i in range(len))
 
 
+def sentry_handler():
+    from raven.contrib.django.raven_compat.models import client
+
+    def send_to_sentry(job, *exc_info):
+        client.captureException(
+            exc_info=exc_info,
+            extra={
+                "job_id": job.id,
+                "func": job.func_name,
+                "args": job.args,
+                "kwargs": job.kwargs,
+                "description": job.description,
+            },
+        )
+
+    return send_to_sentry
+
+
 class Service:
     def start(self):
         import strut
@@ -18,4 +36,9 @@ class Service:
 
         queues = ["default"]
         with Connection():
-            Worker(map(Queue, queues), default_result_ttl=0, name=get_name()).work()
+            Worker(
+                map(Queue, queues),
+                exception_handlers=[sentry_handler()],
+                default_result_ttl=0,
+                name=get_name(),
+            ).work()

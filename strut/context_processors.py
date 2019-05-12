7cb4366529cabab846ac85833669876193af3a5a
Matt Robenolt <matt@ydekproductions.com>
2019-05-12 15:18:32 -0700

    Move preact config to a variable

---
+++ strut/context_processors.py
@@ -0,0 +1,2 @@
+def preact(request):
+    return {"preact": "vendor/preact/10.0.0-beta.1/preact.umd.js"}
--- strut/settings.py
+++ strut/settings.py
@@ -94,7 +94,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.template.context_processors.static",
                 "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+                "strut.context_processors.preact",
             ],
             "builtins": [
                 "django.contrib.staticfiles.templatetags.staticfiles",

516a6436a1899a44b7ef386db2b4db31b8c6e38a
Matt Robenolt <matt@ydekproductions.com>
2019-08-18 20:17:58 -0700

    #neverupgrade

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7.3-alpine3.8
+FROM python:3.7.4-alpine3.10
 
 RUN addgroup -S strut && adduser -S -G strut strut
 
--- tasks.py
+++ tasks.py
@@ -236,7 +236,7 @@ def neverupgrade(ctx):
     for tags in requests.get(
         "https://registry.hub.docker.com/v1/repositories/python/tags"
     ).json():
-        m = re.match(r"^3\.7\.\d+\-alpine3\.8$", tags["name"])
+        m = re.match(r"^3\.7\.\d+\-alpine3\.10$", tags["name"])
         if m:
             v = parse_version(tags["name"].split("-", 1)[0])
             if version is None or v > version:
@@ -246,7 +246,7 @@ def neverupgrade(ctx):
         df = fp.read().splitlines()
 
     with open("Dockerfile", "w") as fp:
-        df[0] = f"FROM python:{str(v)}-alpine3.8"
+        df[0] = f"FROM python:{str(v)}-alpine3.10"
         fp.write("\n".join(df) + "\n")
 
     ctx.run("poetry update")
